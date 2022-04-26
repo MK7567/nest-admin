22996265a23b8966a608c93454dc9bfc644c59ae
MK <matic33.klemencic@gmail.com>
2022-04-26 10:51:14 +0200

    Initial commit

---
+++ src/app.module.ts
@@ -0,0 +1,26 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { UserModule } from './user/user.module';
+import { AuthModule } from './auth/auth.module';
+
+@Module({
+  imports: [
+    UserModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'db',
+      port: 5432,
+      username: 'root',
+      password: 'root',
+      database: 'admin',
+      autoLoadEntities: true, //DO NOT USE ON PRODUCTION
+      synchronize: true,
+    }),
+    AuthModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
