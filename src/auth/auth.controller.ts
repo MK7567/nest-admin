4a4359f39c9befe3896a3bc383a1ef4faaa63700
MK <matic33.klemencic@gmail.com>
2022-04-26 13:22:45 +0200

    added login methods, jwt, password interceptor, logout method

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,12 +1,30 @@
-import { BadRequestException, Body, Controller, Post } from "@nestjs/common";
+import {
+  BadRequestException,
+  Body, ClassSerializerInterceptor,
+  Controller,
+  Get,
+  NotFoundException,
+  Post,
+  Req,
+  Res,
+  UseInterceptors
+} from "@nestjs/common";
 import { UserService } from "../user/user.service";
 import * as bcrypt from 'bcryptjs';
 import { RegisterDto } from "./models/register.dto";
+import { JwtService } from "@nestjs/jwt";
+import { Response } from "express";
+import { Request } from "express";
 
+
+@UseInterceptors(ClassSerializerInterceptor)
 @Controller()
 export class AuthController {
 
-  constructor(private userService: UserService) {
+  constructor(
+    private userService: UserService,
+    private jwtService: JwtService
+  ) {
   }
 
   @Post('register')
@@ -26,4 +44,45 @@ export class AuthController {
   }
 
 
+  @Post('login')
+  async login(
+    @Body('email') email: string,
+    @Body('password') password: string,
+    @Res({passthrough: true}) response: Response
+  ) {
+    const user = await this.userService.findOne({ email });
+
+    if(!user) {
+      throw new NotFoundException('User not found');
+    }
+
+    if(!await bcrypt.compare(password, user.password)){
+      throw new BadRequestException('Invalid credentials');
+    }
+
+    const jwt = await this.jwtService.signAsync({id: user.id});
+    response.cookie('jwt', jwt, { httpOnly: true });
+
+    return user;
+  }
+
+
+  @Get('user')
+  async user(
+    @Req() request: Request
+  ){
+    const cookie = request.cookies['jwt'];
+    const data = await  this.jwtService.verifyAsync(cookie);
+
+    return this.userService.findOne({ id: data.id});
+  }
+
+
+  @Post('logout')
+  async logout(@Res({passthrough: true}) response: Response) {
+    response.clearCookie('jwt');
+    return{
+      message: 'success'
+    }
+  }
 }
