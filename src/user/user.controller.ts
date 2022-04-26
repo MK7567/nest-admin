a32412387aa8c7350cdbf4bcfb4b7b85b92a839e
MK <matic33.klemencic@gmail.com>
2022-04-26 14:23:35 +0200

    added auth guard, common module, CRUD operations for user

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,7 +1,23 @@
-import { Controller, Get } from "@nestjs/common";
+import {
+  Body,
+  ClassSerializerInterceptor,
+  Controller, Delete,
+  Get,
+  Param,
+  Post, Put,
+  UseGuards,
+  UseInterceptors
+} from "@nestjs/common";
 import { UserService } from "./user.service";
 import { User } from "./models/user.entity";
+import * as bcrypt from 'bcryptjs';
+import { UserCreateDto } from "./models/user.create.dto";
+import { AuthGuard } from "../auth/auth.guard";
+import { UserUpdateDto } from "./models/user.update.dto";
 
+
+@UseInterceptors(ClassSerializerInterceptor)
+@UseGuards(AuthGuard)
 @Controller('users')
 export class UserController {
 
@@ -12,4 +28,39 @@ export class UserController {
   async all(): Promise<User[]>{
     return this.userService.all();
   }
+
+  @Post()
+  async create(@Body() body: UserCreateDto): Promise<User> {
+    const password = await bcrypt.hash('1234', 12)
+    return this.userService.create({
+      first_name: body.first_name,
+      last_name: body.last_name,
+      email: body.email,
+      password
+    });
+  }
+
+
+  @Get(':id')
+  async get(@Param('id') id: number){
+    return this.userService.findOne({id})
+  }
+
+
+  @Put(':id')
+  async  update(
+    @Param('id') id: number,
+    @Body() body: UserUpdateDto
+  ){
+    await this.userService.update(id, body)
+
+    return this.userService.findOne({id})
+  }
+
+
+  @Delete(':id')
+  async delete(@Param('id') id: number){
+    return this.userService.delete(id);
+  }
+
 }
