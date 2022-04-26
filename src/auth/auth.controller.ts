133bae09bb4a74a1a91aaa75f665eba49519ac89
MK <matic33.klemencic@gmail.com>
2022-04-26 12:01:56 +0200

    added password hashing, user data validation

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,5 +1,7 @@
-import { Body, Controller, Post } from "@nestjs/common";
+import { BadRequestException, Body, Controller, Post } from "@nestjs/common";
 import { UserService } from "../user/user.service";
+import * as bcrypt from 'bcryptjs';
+import { RegisterDto } from "./models/register.dto";
 
 @Controller()
 export class AuthController {
@@ -8,8 +10,19 @@ export class AuthController {
   }
 
   @Post('register')
-  async register(@Body() body){
-    return this.userService.create(body);
+  async register(@Body() body: RegisterDto){
+    if(body.password !== body.password_confirm){
+      throw new BadRequestException('passwords do not match');
+    }
+
+    const hashed = await bcrypt.hash(body.password, 12);
+
+    return this.userService.create({
+      first_name: body.first_name,
+      last_name: body.last_name,
+      email: body.email,
+      password: hashed,
+    });
   }
 
 
